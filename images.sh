82dd19ac2842500287a4f2e450aa23e7e534c2e5
BrunoMSong <50726279+BrunoMSong@users.noreply.github.com>
2023-03-10 17:51:12 +0800

    Update images.sh

--- images.sh
+++ images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
   
-images=(kube-apiserver:v1.23.17 kube-controller-manager:v1.23.17 kube-scheduler:v1.23.17)
+images=(kube-apiserver:v1.24.11 kube-controller-manager:v1.24.11 kube-scheduler:v1.24.11 kube-proxy:v1.24.11)
 docker login --username=finger_711 registry.cn-hangzhou.aliyuncs.com
 ##获取阿里云仓库的账号密码，方便后续登陆 
 #cho “请输入容器仓库登陆账号：”
