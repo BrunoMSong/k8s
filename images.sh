f7f5cf0a59b37cf29432b2c170c83cda7cc4ab49
BrunoMSong <50726279+BrunoMSong@users.noreply.github.com>
2023-03-30 17:22:42 +0800

    Update images.sh

--- images.sh
+++ images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #定义需要下载的镜像列表
-images=(kube-apiserver:v1.24.11 kube-controller-manager:v1.24.11 kube-scheduler:v1.24.11 kube-proxy:v1.24.11)
+images=(nfsplugin:v4.2.0)
 #登陆阿里云镜像仓库，便于push镜像
 docker login --username=finger_711 registry.cn-hangzhou.aliyuncs.com
 #定义源和目的镜像仓库，便于拼接完整镜像名称
-srcrepo="registry.k8s.io/"
+srcrepo="registry.k8s.io/sig-storage/"
 destrepo="registry.cn-hangzhou.aliyuncs.com/finger-k8s/"
 for i in "${images[@]}" ; do
     docker pull $srcrepo$i
